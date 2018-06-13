ed81175ee8f456e29016cae430b7b0eefb1778d1
Ant Kaynak <antkaynak1@gmail.com>
2018-06-13 21:09:38 +0300

    added views and partials

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 
 const express = require('express');
+const hbs = require('hbs');
 
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear().toString()
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear().toString()
+    });
+});
+
+app.listen('8080');
