1f6dfbbf0e263ccea13534d486bbf65f9a94b001
Ant Kaynak <antkaynak1@gmail.com>
2018-06-13 21:20:04 +0300

    added hbs helper

--- server.js
+++ server.js
@@ -7,17 +7,21 @@ var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
 app.get('/', (req,res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear().toString()
+        //currentYear: new Date().getFullYear().toString()
     });
 });
 
 app.get('/about', (req,res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear().toString()
+        //currentYear: new Date().getFullYear().toString()
     });
 });
 
